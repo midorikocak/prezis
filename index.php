89f5c3d7a04a3265206c1e8a2feec6490cdcb77d
Midori Koçak <mtkocak@mtkocak.net>
2016-08-10 14:58:30 +0200

    added cors

--- index.php
+++ index.php
@@ -15,7 +15,15 @@ $settings = [
     ],
 ];
 
+$corsOptions = array(
+    "origin" => "*",
+    "exposeHeaders" => array("Content-Type", "X-Requested-With", "X-authentication", "X-client"),
+    "allowMethods" => array('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+);
+$cors = new \CorsSlim\CorsSlim($corsOptions);
+
 $app = new Slim\App($settings);
+$app->add($cors);
 
 $container = $app->getContainer();
 
